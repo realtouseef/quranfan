7094b90e515b680a3e616ce95f45d20c4c8bac8c
touseefcodes <touseef69r@gmail.com>
2022-03-18 01:33:07 +0500

    changed description

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,8 @@ export default function Home() {
         <meta charSet="utf-8" />
         <meta
           name="description"
-          content="Feelings are not always good or bad. Sometimes we feel sad, hopeless, or depressed. When this happens, it is natural to want to pray. But what verses should you recite? quranfan tells you."
+          // content="Feelings are not always good or bad. Sometimes we feel sad, hopeless, or depressed. When this happens, it is natural to want to pray. But what verses should you recite? quranfan tells you."
+          content="QuranFan is a beautiful website and has a very different theme. The basic motto behind this platform is to give users a perfect dua that correlates with their current feelings. Upon selecting a specific mood, it automatically gives you a verse from the Quran or a prayer that helps you to cope with it."
         />
         <meta name="robots" content="index, follow" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
