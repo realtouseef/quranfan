d3c56d471bb1b0e4c24337ebc8b044ad4a188d3c
touseefcodes <touseef69r@gmail.com>
2022-03-18 01:23:16 +0500

    added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,35 @@
-import Layout from "../components/Layout"
-import "../styles/globals.css"
+import Layout from "../components/Layout";
+import "../styles/globals.css";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+import * as ga from "../lib/ga";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url);
+    };
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <Layout>
       <div className="selection:bg-pink-400 selection:text-white">
         <Component {...pageProps} />
       </div>
     </Layout>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,6 @@
-import Document, { Html, Head, Main, NextScript } from "next/document"
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
-class MyDocument extends Document {
+export default class MyDocument extends Document {
   render() {
     return (
       <Html>
@@ -13,14 +13,29 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400&display=swap"
             rel="stylesheet"
           />
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
-
-export default MyDocument
